a1c3ced498e0b4847f7eb0ce4cff4e8112c58275
oliveryao09 <695353020@qq.com>
2022-06-11 19:27:07 +1200

    Create menus and the sidebar
    Create multiple menu in the sidebar and create an ability for the user to close the sidebar

--- src/App.js
+++ src/App.js
@@ -22,10 +22,13 @@ import {
   Pyramid,
   Stacked,
 } from './pages';
+
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+  // const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
